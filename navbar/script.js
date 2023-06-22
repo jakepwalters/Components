2b7855791edb8a72a95219509daf69acd9a2af1d
Jake Walters <jakepwalters@outlook.com>
2023-06-22 20:44:48 -0300

    finished functionality

--- navbar/script.js
+++ navbar/script.js
@@ -1,14 +1,39 @@
 const mobileBtn = document.querySelector(".navbar__mobile--btn");
-const mobileMenu = document.querySelector(".navbar__mobile--items");
+const mobileMenu = document.querySelector(".navbar__mobile");
+const mobileMenuItems = document.querySelectorAll(
+	".navbar__mobile--items--link"
+);
 const mobileLogo = document.querySelector(".navbar__mobile--logo");
+const mobileClose = document.querySelector(".navbar__mobile--close");
+const regMenu = document.querySelector(".navbar__reg");
 
 function activateMobile() {
-	mobileMenu.classList.toggle("disableMobile");
-	// mobileBtn.classList.toggle("disabledMobile");
-	// mobileLogo.classList.toggle("disabledMobile");
-	mobileBtn.style.display = "none";
-	mobileLogo.style.display = "none";
-	mobileMenu.classList.toggle("activateMobile");
+	mobileBtn.classList.toggle("disabled");
+	mobileLogo.classList.toggle("disabled");
+	mobileClose.classList.toggle("disabled");
+	mobileMenuItems.forEach((e) => e.classList.toggle("disabled"));
+}
+function deactivateMobile() {
+	mobileBtn.classList.toggle("disabled");
+	mobileLogo.classList.toggle("disabled");
+	mobileClose.classList.toggle("disabled");
+	mobileMenuItems.forEach((e) => e.classList.toggle("disabled"));
 }
 
 mobileBtn.addEventListener("click", activateMobile);
+mobileClose.addEventListener("click", deactivateMobile);
+
+function showMobile(size) {
+	if (size.matches) {
+		mobileMenu.style.display = "grid";
+		regMenu.style.display = "none";
+	} else {
+		mobileMenu.style.display = "none";
+		regMenu.style.display = "grid";
+	}
+}
+
+const size = window.matchMedia("(max-width: 768px)");
+showMobile(size);
+
+size.addEventListener("change", showMobile);
